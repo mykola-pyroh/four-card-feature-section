d221b046f5ec393745b3fe8aae4934148c4a3920
Mykola Pyroh <40759506+mykola-pyroh@users.noreply.github.com>
2025-01-16 01:05:10 +0200

    Refactor SCSS rules, added Card styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,10 +3,12 @@ import "./Card.scss";
 export default function Card({ title, summary, icon }: { title: string; summary: string; icon: string }) {
     
     return (
-        <div className="card">
-            <p>{title}</p>
-            <p>{summary}</p>
-            <img src={icon} alt="Icon" />
-        </div>
+        <article className="card">
+            <div>
+                <p className="card__title">{title}</p>
+                <p className="card__summary">{summary}</p>
+            </div>
+            <img src={icon} alt={`${title} icon`} className="card__icon" />
+        </article>
     )
 }
