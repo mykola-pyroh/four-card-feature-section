a0a0c95548ce64addf94ef01dee7ae7aab8da6cc
Mykola Pyroh <mykola.pyroh@ukr.net>
2025-01-16 12:12:29 +0200

    Added classes for Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,9 +7,9 @@ export default function Main() {
     return (
         <main>
             <section className="title-section">
-                <p>Reliable, efficient delivery</p>
-                <p>Powered by Technology</p>
-                <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+                <p className="title-section__subtitle">Reliable, efficient delivery</p>
+                <p className="title-section__title">Powered by Technology</p>
+                <p className="title-section__summary">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
             </section>
             <section className="card-section">
                 {data.map(el => <Card key={el.title} title={el.title} summary={el.summary} icon={el.icon} />)}
